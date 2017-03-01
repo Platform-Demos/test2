6a16777ab94c527759407ec59285a461ff40faad
padmakaw <padmaka.wijayagoonawardena@trycake.com>
2017-03-01 18:22:29 +0530

    pom

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+/**
+ * @author : Padmaka Wijayagoonawardena
+ * @date : 3/1/17
+ */
+public class Main {
+    private static Main ourInstance = new Main();
+
+    public static Main getInstance() {
+        return ourInstance;
+    }
+
+    private Main() {
+        System.out.println("something");
+        System.out.println("something");
+    }
+}
